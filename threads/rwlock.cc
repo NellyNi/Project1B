05c5ff67f1cabf0d952c7a849221ef247c6324a4
Jiawei Ni <jiawei_ni@cs.ucsb.edu>
2020-01-31 11:26:33 -0800

    Task3 fail

--- threads/rwlock.cc
+++ threads/rwlock.cc
@@ -8,11 +8,78 @@
 //Your solution for  Task 3 
 //TODO
 
-RWLock::RWLock(){ }
+RWLock::RWLock(){ 
+    char *lckname = "L"; 
+    lck=new Lock(lckname); 
+    
+    char *Rconname = "R"; 
+    Rcondition=new Condition(Rconname); 
+    
+    char *Wconname = "W"; 
+    Wcondition=new Condition(Rconname); 
+    
+    waitReadNumber = 0;
+    readingNumber = 0;
+    waitWriteNumber = 0;
+    writingNumber = 0;
+}
 
-RWLock::~RWLock(){ }
+RWLock::~RWLock(){ 
+    delete[] lck;
+    delete[] Rcondition;
+    delete[] Wcondition;
+}
 
-void RWLock::startRead(){ }
-void RWLock::doneRead(){ }
-void RWLock::startWrite(){ }
-void RWLock::doneWrite(){ }
+void RWLock::startRead(){ 
+    lck->Acquire();
+    waitReadNumber++;
+    while(readShouldWait()){
+        Rcondition->Wait(lck);
+    }
+    waitReadNumber--;
+    readingNumber++;
+    lck->Release();
+}
+void RWLock::doneRead(){ 
+    lck->Acquire();
+    readingNumber--;
+    if(!readShouldWait()){
+        Wcondition->Signal(lck);
+    }
+    lck->Release();
+}
+void RWLock::startWrite(){ 
+    lck->Acquire();
+    waitWriteNumber++;
+    while(writeShouldWait()){
+        Wcondition->Wait(lck);
+    }
+    waitWriteNumber--;
+    writingNumber++;
+    lck->Release();
+}
+void RWLock::doneWrite(){ 
+    lck->Acquire();
+    writingNumber--;
+    if(waitWriteNumber > 0){
+        Wcondition->Wait(lck);
+    }
+    else if(waitReadNumber > 0){
+        Rcondition->Broadcast(lck);
+    }
+    lck->Release();
+}
+
+bool RWLock::readShouldWait(){
+  if(writingNumber > 0 || waitWriteNumber > 0){
+    return true;
+  }
+  return false;
+}
+
+bool RWLock::writeShouldWait(){
+  if(writingNumber > 0 || readingNumber > 0){
+    return true;
+  }
+  return false;
+}
--- threads/rwlock.h
+++ threads/rwlock.h
@@ -2,11 +2,21 @@
 #define __RWLOCK_H__
 #include<semaphore.h>
 #include "synch.h"
+#define HTABLE_SIZE 128
 
 class RWLock{
 private:
       //Add  fields for  RWLock. Your solution to Task 3
       //TODO
+		Lock  *lck;
+		Condition  *Rcondition;
+		Condition  *Wcondition;
+		int waitReadNumber;
+		int readingNumber;
+		int waitWriteNumber;
+		int writingNumber;
+		bool readShouldWait();
+		bool writeShouldWait();
 public:
     	RWLock();
     	~RWLock();
